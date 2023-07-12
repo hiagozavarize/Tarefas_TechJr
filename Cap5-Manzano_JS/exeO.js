c46eb2b071115cb886f2a99be07dae2bb0f23aa2
Hiago <78562407+hiagozavarizex2@users.noreply.github.com>
2023-07-12 19:49:33 -0300

    ajuste no calculo do fatorial

--- Cap5-Manzano_JS/exeO.js
+++ Cap5-Manzano_JS/exeO.js
@@ -1,14 +1,11 @@
 const prompt = require("prompt-sync")();
 
-let fat = 1, num;
+let fat, num;
 
-for(i = 1; i <= 10; i++){
-    if(i % 2 != 0){
-        for(num = i; num >= 1; num--){
-          fat *= num;
-        }
-        console.log("o fatorial de %d", i, fat);
-    }else{
-        continue;
+for(i = 1; i <= 10; i += 2){
+    fat = 1
+    for(num = i; num >= 1; num--){
+      fat *= num;
     }
+    console.log("o fatorial de %d", i, fat);
 }
