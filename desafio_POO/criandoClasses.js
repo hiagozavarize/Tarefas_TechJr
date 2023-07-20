d294b742ab611d494348d25808e48105e63fbcd1
Hiago <78562407+hiagozavarizex2@users.noreply.github.com>
2023-07-20 12:39:01 -0300

    done

--- desafio_POO/criandoClasses.js
+++ desafio_POO/criandoClasses.js
@@ -4,10 +4,10 @@
 1. Crie uma classe chamada `Pessoa` que tenha dois campos, `nome` e `idade`. #
 2. Crie alguns métodos para pegar e definir o nome e a idade da pessoa. #
 3. Crie duas instâncias da classe `Pessoa`, `pessoa1` e `pessoa2`. #
-4. Use as instâncias para definir os nomes e as idades das pessoas e, em seguida, imprima os nomes e as idades das pessoas na tela.
+4. Use as instâncias para definir os nomes e as idades das pessoas e, em seguida, imprima os nomes e as idades das pessoas na tela.#
 5. Adicione um método chamado `falar` que imprima o nome da pessoa. #
 6. Adicione um método chamado `fazerAniversario` que incrementa a idade da pessoa e imprime uma mensagem de aniversário. #
-7. Use as instâncias para definir os nomes e as idades das pessoas, falar, fazer aniversário e imprimir os nomes e as idades das pessoas na tela.
+7. Use as instâncias para definir os nomes e as idades das pessoas, falar, fazer aniversário e imprimir os nomes e as idades das pessoas na tela.#
 */
 
 const prompt = require("prompt-sync")();
